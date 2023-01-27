accf4cdcd2332e10918df7f233eb341bca76f3e5
CAPYBARA <dltjdrnjs3@naver.com>
2023-01-27 18:33:21 +0900

    JoinEvnet edit11
    created onguildmember Join, onMessageReceived event
    
    deleted previous overrided method in JoinEvent class

--- src/main/java/com/kojubu/event/JoinEvent.java
+++ src/main/java/com/kojubu/event/JoinEvent.java
@@ -1,22 +1,31 @@
 package com.kojubu.event;
 
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import net.dv8tion.jda.api.entities.channel.unions.MessageChannelUnion;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.List;
+
 public class JoinEvent extends ListenerAdapter {
     @Override
-    public void onGuildJoin(GuildJoinEvent event) {
-        super.onGuildJoin(event);
+    public void onGuildMemberJoin(GuildMemberJoinEvent event) {
+        String MemberMention=event.getUser().getAsMention();
+        final List<TextChannel> FindWelcomeChannel = event.getGuild().getTextChannelsByName("welcome", true);
+        TextChannel WelcomeChannel = FindWelcomeChannel.get(0);
+        String WelcomeMessage = String.format("**반가워요!**, "+MemberMention+"님!");
+        WelcomeChannel.sendMessage(WelcomeMessage).queue();
     }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
-        int k;
-        String currentchannelname= event.getChannel().getName();
-        MessageChannelUnion Channel = event.getChannel();
-        super.onMessageReceived(event);
+        int k=0;
+        if(event.getMessage().equals("이동훈")){
+            k++;
+            event.getChannel().sendMessage("귀여운 기니피그\n").addContent("적은 횟수"+k).queue();
+        }
     }
 
 }
